ef61a6a247e55408e3db1e5e87bebfbef95dace1
igameshat <itayhrs@gmail.com>
2025-02-12 21:34:30 +0200

    Add files via upload

---
+++ IstiertaggerClient.java
@@ -0,0 +1,10 @@
+package com.example.tag;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IstiertaggerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
